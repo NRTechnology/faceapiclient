77a4c29757af96fc878773482aec179561853582
LENOVO <nrtechnologyid@gmail.com>
2022-06-06 11:54:03 +0700

    webcam

--- main.py
+++ main.py
@@ -4,15 +4,22 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from requests.exceptions import ContentDecodingError, RequestException, HTTPError, ConnectionError, Timeout
 from datetime import datetime
 from logging import basicConfig, info, error, INFO
+import cv2
+import cvlib as cv
+import time
+
 
 packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
-def send_image():
+def send_image(endpoint_url, file_path):
+    # file_path = 'C:\\Users\\LENOVO\\PycharmProjects\\facedetection\\laki.jpg'
+    file_name = os.path.basename(file_path)
+
     try:
         request = Request(
             'POST',
-            endpoint,
+            endpoint_url,
             files={
                 'ImageFile': (file_name, open(file_path, 'rb'), 'image/jpeg', {'Expires': '0'}),
             }
@@ -51,10 +58,40 @@ if __name__ == '__main__':
                 level=INFO)
     info("---start log---")
 
-    file_path = 'C:\\Users\\LENOVO\\PycharmProjects\\facedetection\\laki.jpg'
-    file_name = os.path.basename(file_path)
     endpoint = 'http://localhost:8080/api/upload/'
 
-    send_image()
+    vid = cv2.VideoCapture(0)
+    while (True):
+        time.sleep(1)
+        # Capture the video frame
+        # by frame
+        ret, frame = vid.read()
+
+        # Display the resulting frame
+        face, confidence = cv.detect_face(frame)
+
+        for idx, f in enumerate(face):
+            # get corner points of face rectangle
+            (startX, startY) = f[0], f[1]
+            (endX, endY) = f[2], f[3]
+
+            # draw rectangle over face
+            cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
+            cv2.imwrite("face.jpg", frame)
+            send_image(endpoint_url=endpoint, file_path='face.jpg')
+
+        cv2.imshow('frame', frame)
+        # the 'q' button is set as the
+        # quitting button you may use any
+        # desired button of your choice
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # After the loop release the cap object
+    vid.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
+
+    # send_image(endpoint_url=endpoint)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
